f945a76edda47bea1c9ab12a17becc57578599a1
DIC <chlehddlf94@gmail.com>
2022-06-23 16:18:50 +0900

    bonjok-spring-website 22.06.23 16:17

---
+++ src/main/java/com/bonjok/www/BonjokWebpageApplication.java
@@ -0,0 +1,13 @@
+package com.bonjok.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonjokWebpageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BonjokWebpageApplication.class, args);
+    }
+
+}
