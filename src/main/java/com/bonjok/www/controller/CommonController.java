eac55740c8af9a75a6996c3b04c7bba8957f91c3
kcoderrr <chlehddlf94@gmail.com>
2022-06-27 17:50:44 +0900

    6/27 17:50 commit

--- src/main/java/com/bonjok/www/controller/CommonController.java
+++ src/main/java/com/bonjok/www/controller/CommonController.java
@@ -1,8 +1,12 @@
 package com.bonjok.www.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 
 @Controller
@@ -14,5 +18,9 @@ public class CommonController {
         return "./commons/signin";
     }
 
+    @PostMapping("signin")
+    public String signin(Model model) {
+        return "";
+    }
 
 }
